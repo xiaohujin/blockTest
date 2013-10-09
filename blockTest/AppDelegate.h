f3d9bd94db224944cc84eaa50f6305195f56ebf7
xiaohujin <xiaohujin@xiaohujintekiMac.local>
2013-10-09 11:21:37 +0800

    Initial Commit

---
+++ blockTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blockTest
+//
+//  Created by xiaohujin on 13-10-9.
+//  Copyright (c) 2013年 xiaohujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blockTestTests/blockTestTests.m
@@ -0,0 +1,34 @@
+//
+//  blockTestTests.m
+//  blockTestTests
+//
+//  Created by xiaohujin on 13-10-9.
+//  Copyright (c) 2013年 xiaohujin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface blockTestTests : XCTestCase
+
+@end
+
+@implementation blockTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
