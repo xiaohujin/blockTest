966c9d28234a93f1ed413b3e82e90c609a640566
xiaohujin <xiaohujin@xiaohujintekiMac.local>
2013-10-09 11:24:39 +0800

    init the project .

---
+++ blockTest/Classes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  blockTest
+//
+//  Created by xiaohujin on 13-10-9.
+//  Copyright (c) 2013年 xiaohujin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
